b8b26f907c77686669bc4a28576708d0b3a256a2
Myeongseok Baek <codetemplate@hanmail.net>
2023-06-11 15:54:41 +0900

    project created

---
+++ src/main/java/com/example/presenting_tdd/PresentingTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.presenting_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentingTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentingTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/presenting_tdd/PresentingTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.presenting_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentingTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

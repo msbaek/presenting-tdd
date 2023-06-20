bc13ad2fb8a3fcabea2e9300664b55008c435755
Myeongseok Baek <codetemplate@hanmail.net>
2023-06-20 12:08:51 +0900

    enhance ConverySpecificationTest

--- src/test/java/com/example/presenting_tdd/ConverySpecificationTest.java
+++ src/test/java/com/example/presenting_tdd/ConverySpecificationTest.java
@@ -1,5 +1,7 @@
 package com.example.presenting_tdd;
 
+import groovy.lang.Tuple2;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -47,23 +49,14 @@ enum ComparisonType {
 }
 
 public class ConverySpecificationTest {
-    @Test
-    public void test() {
-        final Conveyor conveyor2 = new Conveyor(DeliveryType.INTERNATIONAL, false, 5, 1, 6.9);
-        final Conveyor conveyor3 = new Conveyor(DeliveryType.INTERNATIONAL, false, 6, 1, 6.9);
-        final Conveyor conveyor4 = new Conveyor(DeliveryType.DOMESTIC, false, 3, 1, 6.9);
-        final Conveyor conveyor5 = new Conveyor(DeliveryType.DOMESTIC, false, 4, 1, 6.9);
 
-        assertThat(matchConveyorLine(conveyor2)).isEqualTo("2");
-        assertThat(matchConveyorLine(conveyor3)).isEqualTo("3");
-        assertThat(matchConveyorLine(conveyor4)).isEqualTo("4");
-        assertThat(matchConveyorLine(conveyor5)).isEqualTo("5");
-    }
+    private Map<String, GmsSpecification<Conveyor>> conveyorLines;
 
-    private String matchConveyorLine(final Conveyor conveyor5) {
+    @BeforeEach
+    void setUp() {
         // read ConveyorLineCriteria from DB
         // create conveyorLines
-        Map<String, List<GmsSpecification>> conveyorLines = new HashMap<>();
+        conveyorLines = new HashMap<>();
         conveyorLines.put("2", createConveyorSpecifications(
                 new ConveyorLineCriteria(DeliveryType.INTERNATIONAL, false, LTE, 5, Arrays.asList(1, 2), LT, 7.0)));
         conveyorLines.put("3", createConveyorSpecifications(
@@ -72,24 +65,39 @@ public class ConverySpecificationTest {
                 new ConveyorLineCriteria(DeliveryType.DOMESTIC, false, LTE, 3, Arrays.asList(1, 2), LT, 7.0)));
         conveyorLines.put("5", createConveyorSpecifications(
                 new ConveyorLineCriteria(DeliveryType.DOMESTIC, false, GT, 3, Arrays.asList(1, 2), LT, 7.0)));
-        for (Map.Entry<String, List<GmsSpecification>> entry : conveyorLines.entrySet()) {
-            String key = entry.getKey();
-            List<GmsSpecification> value = entry.getValue();
-            boolean b = value.stream().allMatch(s -> s.isSatisfiedBy(conveyor5));
-            if (b)
-                return key;
+    }
+
+    @Test
+    public void test() {
+        List<Tuple2<String, Conveyor>> conveyors = List.of(
+                new Tuple2<>("2", new Conveyor(DeliveryType.INTERNATIONAL, false, 5, 1, 6.9)),
+                new Tuple2<>("3", new Conveyor(DeliveryType.INTERNATIONAL, false, 6, 1, 6.9)),
+                new Tuple2<>("4", new Conveyor(DeliveryType.DOMESTIC, false, 3, 1, 6.9)),
+                new Tuple2<>("5", new Conveyor(DeliveryType.DOMESTIC, false, 4, 1, 6.9))
+        );
+        for (final Tuple2<String, Conveyor> conveyor : conveyors) {
+            assertThat(matchConveyorLine(conveyor.getV2())).isEqualTo(conveyor.getV1());
+        }
+    }
+
+    private String matchConveyorLine(final Conveyor conveyor5) {
+        for (Map.Entry<String, GmsSpecification<Conveyor>> entry : conveyorLines.entrySet()) {
+            String k = entry.getKey();
+            GmsSpecification<Conveyor> v = entry.getValue();
+            if(v.isSatisfiedBy(conveyor5))
+                return k;
         }
         return "unmatched";
     }
 
-    private List<GmsSpecification> createConveyorSpecifications(final ConveyorLineCriteria conveyorLineCriteria) {
-        List<GmsSpecification> specifications = new ArrayList<>();
-        specifications.add(new ConveyorSpecification(c -> c.deliveryType() == conveyorLineCriteria.deliveryType()));
-        specifications.add(new ConveyorSpecification(c -> c.isTube() == conveyorLineCriteria.isTube()));
-        specifications.add(new ConveyorSpecification(c -> conveyorLineCriteria.itemCountComparisonType().compare(c.itemCount(), conveyorLineCriteria.itemCountThreshold())));
-        specifications.add(new ConveyorSpecification(c -> conveyorLineCriteria.excludeBoxCodes().contains(c.boxCode())));
-        specifications.add(new ConveyorSpecification(c -> conveyorLineCriteria.weightComparisonType().compare(c.weight(), conveyorLineCriteria.weightThreshold())));
-        return specifications;
+    private GmsSpecification<Conveyor> createConveyorSpecifications(final ConveyorLineCriteria conveyorLineCriteria) {
+        List<GmsSpecification<Conveyor>> specifications = new ArrayList<>();
+        specifications.add(new ConveyorSpecification<Conveyor>(c -> c.deliveryType() == conveyorLineCriteria.deliveryType()));
+        specifications.add(new ConveyorSpecification<Conveyor>(c -> c.isTube() == conveyorLineCriteria.isTube()));
+        specifications.add(new ConveyorSpecification<Conveyor>(c -> conveyorLineCriteria.itemCountComparisonType().compare(c.itemCount(), conveyorLineCriteria.itemCountThreshold())));
+        specifications.add(new ConveyorSpecification<Conveyor>(c -> conveyorLineCriteria.excludeBoxCodes().contains(c.boxCode())));
+        specifications.add(new ConveyorSpecification<Conveyor>(c -> conveyorLineCriteria.weightComparisonType().compare(c.weight(), conveyorLineCriteria.weightThreshold())));
+        return new CompositeSpecification<Conveyor>(specifications);
     }
 }
 
@@ -101,15 +109,29 @@ interface GmsSpecification<T> {
     boolean isSatisfiedBy(T t);
 }
 
-class ConveyorSpecification implements GmsSpecification<Conveyor> {
+class ConveyorSpecification<T> implements GmsSpecification<T> {
 
-    private final Predicate<Conveyor> p;
+    private final Predicate<T> p;
 
-    public ConveyorSpecification(Predicate<Conveyor> p) {
+    public ConveyorSpecification(Predicate<T> p) {
         this.p = p;
     }
 
-    public boolean isSatisfiedBy(final Conveyor conveyor) {
+    public boolean isSatisfiedBy(final T conveyor) {
         return p.test(conveyor);
+    }
+}
+
+class CompositeSpecification<T> implements GmsSpecification<T> {
+
+    private final List<GmsSpecification<T>> specifications;
+
+    public CompositeSpecification(final List<GmsSpecification<T>> specifications) {
+        this.specifications = specifications;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(final T t) {
+        return specifications.stream().allMatch(s -> s.isSatisfiedBy(t));
     }
 }
